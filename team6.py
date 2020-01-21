55bb6fdf40e408c49f94a99ce7de6e7d77801ea8
heyitsjasper <59930789+heyitsjasper@users.noreply.github.com>
2020-01-21 10:06:04 -0900

    Update team6.py

--- team6.py
+++ team6.py
@@ -4,9 +4,10 @@
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
-####
+#### 
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Jasper and Ana Maria' # Only 17 chars displayed.
+#### Jasper and Ana Maria
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
