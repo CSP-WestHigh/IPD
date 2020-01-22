d503e22024ff42230cfcf92d756468d1b3c08186
souzor <souza_david@asdk12.org>
2020-01-22 08:19:13 -0900

    Update team14.py

--- team14.py
+++ team14.py
@@ -6,8 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Souzor!' # Only 10 chars displayed.
-# Souza
+team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
