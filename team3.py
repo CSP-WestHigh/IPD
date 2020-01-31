315312d60e1b990a7a666ea3469af499fdae2ffa
souzor <souza_david@asdk12.org>
2020-01-31 08:03:17 -0900

    Update team3.py

--- team3.py
+++ team3.py
@@ -1,13 +1,5 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'Big Money' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'Big Money.' # Only 10 chars displayed.
+strategy_name = 'Winning retaliation'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
@@ -18,15 +10,13 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if their_history == '':
+        return 'b'
+    if their_history[-1] == 'b' and their_history[-2] == 'c':
+        return 'c'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
